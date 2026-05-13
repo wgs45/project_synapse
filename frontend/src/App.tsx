92ed2a859eb0736be356373ae3089cc1e4b5df46
W45 <73011833+wgs45@users.noreply.github.com>
2026-05-13 21:30:23 +0800

    Dockerize project (#8)
    Setup project inside an container for efficient development

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <>
       <div className="text-3xl font-bold underline">Frontend</div>
+      <h1>Built using Vite</h1>
     </>
   );
 }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,12 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true, // This is a shortcut for 0.0.0.0 in Vite
+    port: 3000,
+    strictPort: true, // Keeps the port consistent for Docker
+    watch: {
+      usePolling: true, // Essential for some Docker setups on Windows/Mac to detect file changes
+    },
+  },
 });
